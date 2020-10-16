5e0a55693ec2ab54561ad837a657bfa67d4c249c
Kaue Oliveira <elmovckaue@gmail.com>
2020-10-16 00:54:55 -0400

    Fit purge count to range rather than complain

--- commands/purge.js
+++ commands/purge.js
@@ -1,20 +1,17 @@
-const { selfdestructReply } = require('../util/discord-tools.js')
+const { selfdestructReply } = require('../util/discord-tools.js');
 
 const name = 'purge';
 const description = 'Purge 1 - 100 messages.';
 
-function execute(message, args) {
-    const purgeCount = args;
-    if (purgeCount < 1 || purgeCount > 100) {
-        return selfdestructReply('Invalid count parameter! Must be between 1 and 100.');
-    }
+function execute(message, count) {
+    count = Math.min(Math.max(1, count + 1), 100);
 
     let errorMsg = '';
     message.delete()
         .catch(error => errorMsg += `Couldn't delete command message because of: ${error}`);
-    message.channel.bulkDelete(purgeCount + 1)
+    message.channel.bulkDelete(count)
         .then(() => {
-            selfdestructReply(message, `Deletion of ${purgeCount} message(s) successful.`);
+            selfdestructReply(message, `Deletion of ${count - 1} message(s) successful.`);
         })
         .catch(error => {
             errorMsg += `Couldn't delete messages because of: ${error}`;
