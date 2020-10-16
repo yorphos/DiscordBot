69449d254c8d1617d401268a793324e1dc01f1c8
Kaue Oliveira <elmovckaue@gmail.com>
2020-10-16 00:41:12 -0400

    Add discord-tools for utilities

---
+++ util/discord-tools.js
@@ -0,0 +1,36 @@
+function lastMessageTimestamp(user, guild) { // Fetch unix time of last message sent
+    return guild.members.get(user).lastMessage.createdTimestamp;
+}
+
+function findGuildMember(nametag, guild) { // Search for existing user
+    for (let [snowflake, guildMember] of guild.members) {
+        if (nametag === guildMember.user.tag) {
+            return guildMember;
+        }
+    }
+    return undefined;
+}
+
+function getUserFromMention(mention) {
+    if (!mention) return;
+
+    mention = mention.toString();
+    if (mention.startsWith('<@') && mention.endsWith('>')) {
+        mention = mention.slice(2, -1);
+
+        if (mention.startsWith('!')) {
+            mention = mention.slice(1);
+        }
+
+        return client.users.cache.get(mention);
+    }
+}
+
+async function selfdestructReply(message, text, timeout = 5000) {
+    await message.reply('```' + text + '```')
+        .then(message => {
+            message.delete({ timeout: timeout });
+        });
+}
+
+module.exports = { lastMessageTimestamp, findGuildMember, getUserFromMention, selfdestructReply }
